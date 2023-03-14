f73523f6404f425e8caeeac24511cd72e6d3deba
Crystal Cheung <canttellya6@gmail.com>
2023-03-14 23:43:34 +1100

    Week 3 Work

---
+++ src/pre-compiled/MyUdpServer.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+public class MyUdpServer{
+public static void main(String args[]){ 
+        System.out.println("starting server");
+	DatagramSocket aSocket = null;
+	try{
+		aSocket = new DatagramSocket(6789);
+		byte[] buffer = new byte[1000];
+		String message = "TRUE";
+		while(true){
+			DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(request); 
+			System.out.println("request received");
+			/*if(request.getData == "HELO"){
+				message = "G'DAY";
+			}*/
+			DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+			aSocket.send(reply);
+		}
+	}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+	}catch (IOException e) {System.out.println("IO: " + e.getMessage());
+	}finally {if(aSocket != null) aSocket.close();}
+
+}
+}
