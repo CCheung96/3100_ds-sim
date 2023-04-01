858fe2fe4bb701c7ddaf1aec8ac0d9b739d9c921
Crystal Cheung <canttellya6@gmail.com>
2023-04-01 23:08:06 +1100

    Made styling changes

--- src/pre-compiled/Stage1Client.java
+++ src/pre-compiled/Stage1Client.java
@@ -3,13 +3,12 @@ import java.io.*;
 
 class Stage1Client {
 	static Socket s;
-	static DataInputStream din;
 	static DataOutputStream dout;
 	static BufferedReader in;
 
-	static String response; // reads each line sent from the server
+	static String response; // Temporarily stores messages from server
 
-	// Initialise values for finding the best Server 
+	// Initialise values for storing details of the "best" server 
 	static String serverType = "";
 	static int serverCores = 0;
 	static int serverCount = 0;
@@ -18,63 +17,68 @@ class Stage1Client {
 
 	public static void main(String args[])throws Exception{
 		s=new Socket("localhost",50000);
-		din=new DataInputStream(s.getInputStream());  
 		dout=new DataOutputStream(s.getOutputStream());  
 		in=new BufferedReader(new InputStreamReader(s.getInputStream()));  
 
 		try{
 			String servCommand, jobID;
 
-			// START HANDSHAKE
+			// START HANDSHAKE:
+			// HELO
 			response = callResponse("HELO"); 
-			// serverOutput(response);
-
+			// AUTH crystal
 			String username = System.getProperty("user.name");
 			response = callResponse("AUTH " + username); 
-			// serverOutput(response); 
 			
 			Boolean bestServerFound = false; // Best server not yet found
+
 			// JOBN LOOP
 			 while(!response.equals("NONE")){
 				response = callResponse("REDY");
-				// serverOutput(response);	
-				// break loop if response == "NONE"
+				// Break loop if response indicates no more jobs to schedule or 
+				// complete
 				if(response.equals("NONE")){
 					break;
 				}
 				
-				// store command and jobID
-				String[] arrResponse = response.split(" ");
-				servCommand = arrResponse[0];
-				jobID = arrResponse[2];
+				// Store command and jobID
+				String[] responseArr = response.split(" ");
+				servCommand = responseArr[0];
+				jobID = responseArr[2];
 
 				
-
+				/*
+				 * Run unless "best" server has already been found. 
+				 * bestServerFound set to true if function runs succesfully.
+				 */
 				if(bestServerFound.equals(false)){
-					// System.out.println("Finding Best Server");
-					bestServerFound = findBestServer();
+					bestServerFound = findBestServer(); 
 				}
-				// System.out.println("Best Server: " + serverType);
 				
-
+				// Schedule job if stored command was "JOBN"
 				if (servCommand.equals("JOBN")){
-				
 					Integer serverID = Integer.valueOf(jobID) % serverCount;
-					response = callResponse("SCHD "+ jobID + " " + serverType + " " + serverID);
-					// serverOutput(response); // Should output OK;
+					response = callResponse("SCHD "+ jobID + " " + serverType + 
+						" " + serverID);
 				}
-
 			}
 
+			// Initiate QUIT sequence
 			response = callResponse("QUIT");
-			// serverOutput(response);
-
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 
 
 	}
+
+	 /***
+	  * Writes a message, sends it to the server and receives a single message 
+	  * from the server
+	  * 
+	  * @param request : the message to be sent to the server
+	  * @return : the next line read from the server
+	  ***/
 
 	public static String callResponse(String request) {
 		try {
@@ -87,10 +91,14 @@ class Stage1Client {
 		return "";
 	}
 
-	// public static void serverOutput(String response){
-	// 	System.out.println("Server says: " + response);  
-	// }
-
+	/***
+	 * Goes through the list of servers provided by the server and determines 
+	 * which server Type has the most processing cores. The server type's name 
+	 * and the number of servers of that type are stored to use for scheduling.
+	 * 
+	 * @return : confirmation of whether the search for the "best server" has 
+	 * suceeded (or failed due to an exception interrupt)
+	 ***/
 	public static Boolean findBestServer(){
 		try {
 				response = callResponse("GETS All");
@@ -101,39 +109,44 @@ class Stage1Client {
 				dout.flush();
 				for(int i = 0; i< nRecs; i++){
 					/*
-					 * Server State information is formatted in a single line
-					 * like so:
+					 * Server State information stored in response is formatted 
+					 * in a single line like so:
 					 * 
-					 * serverType serverID state curStartTime serverCores memory disk waitingJobs runningJobs
+					 * serverType serverID state curStartTime serverCores memory
+					 * disk waitingJobs runningJobs
 					 */
 					response= in.readLine();
-					// System.out.println(response);
-
+					
+					// Exit the for loop if "." is encountered rather than 
+					// server information
 					if(response.equals(".")){break;}
 
-					String[] serverApp = response.split(" ");
+					String[] recordArr = response.split(" ");
 
-					if(serverApp[0].equals(serverType)){
+					// Increase the counter if the server is the same server 
+					// type as the biggest server type 
+					if(recordArr[0].equals(serverType)){
 						serverCount++;
 					}
 					// Keep track of the serverType with the largest serverCores
-					if(Integer.valueOf(serverApp[4]) > serverCores){
-						serverType = serverApp[0];
-						serverCores = Integer.valueOf(serverApp[4]);
+					// and reset the server count
+					if(Integer.valueOf(recordArr[4]) > serverCores){
+						serverType = recordArr[0];
+						serverCores = Integer.valueOf(recordArr[4]);
 						serverCount = 1;
 					}
 				}
 
+				// Acknowledgement that all server information is received
 				response = callResponse("OK");
-				// serverOutput(response);	// Should output "."
-
-				return true;
+				// Best server has been found, so set bestServerFound to true
+				return true; 
 
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage());
-			return false;
 		}
-			
-
+		// Best server has not been succesfully found, so bestServerFound 
+		// should remain false
+		return false; 
 	}
 }
